24d19aeaaf3c1f7f6ae6936cc2a7e36947932982
Pedro Afonso <sirafonso005896@outlook.com>
2024-04-06 20:25:09 -0300

    :sparkles: feat: finalização da tarefa 04/04

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
-const titulo = document.getElementById('header')
-const img = document.getElementById('evolucao')
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString); 
-var evolucao = urlParams.get('evolucao')
-evolucao = evolucao.toLowerCase()
+function set_page(evolucao, image_url, target) {
+  evolucao = evolucao.charAt(0).toUpperCase() + evolucao.slice(1)
+  console.log(image_url)
+  target.innerHTML = `
+  <h1>Detalhes sobre o ${evolucao} </h1>
+  
+  <img src="${image_url}" alt=${evolucao}>
+  `
+}
 
-var pokemon
-const response = fetch(`https://pokeapi.co/api/v2/pokemon/${evolucao}`).then((res) => res.json()).then(data => {
+async function getImageURL(evolucao) {
+  const evolucao_url = (await fetch(`https://pokeapi.co/api/v2/pokemon/${evolucao}`)).url;
+  const pokemon_detail_object = await (await fetch(evolucao_url)).json();
+  const image_path = pokemon_detail_object.sprites.front_default
 
-    img.innerHTML = `<h1>Imfromacao sobre ${evolucao} </h1>
-    <img
-    src="${data.sprites.front_shiny}"
-    alt="${evolucao}"
-    id="pokemon-page"
-    />` 
-})
-/* */
+  return image_path;
+}
 
-titulo.innerHTML = `<h1>Pagina do ${evolucao}</h1>`
+async function main() {
+  const titulo = document.getElementById('header')
+  const img = document.getElementById('evolucao')
+  const queryString = window.location.search
+  const evolucao = new URLSearchParams(queryString).get('evolucao').toLowerCase(); 
+
+  titulo.innerHTML = `<h1>Pagina do ${evolucao}</h1>`
+
+  const evolucao_image_url = await getImageURL(evolucao, img);
+  set_page(evolucao, evolucao_image_url, img)
+}
+
+main();
